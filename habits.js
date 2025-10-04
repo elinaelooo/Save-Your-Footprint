54bf8731fca39414d312b8ec95d04ffeb4b992bd
elinaelooo <elinaelina1111@gmail.com>
2025-10-04 11:25:40 -0400

    function for base eco score on interaction with dropdowns
    do the html changes i did so the js works
    please delete davids web art

--- habits.js
+++ habits.js
@@ -1,47 +1,52 @@
 const $ = (s, p = document) => p.querySelector(s);
 
-const f = new FormData($("#habitsForm"))
-
-base = 2.5;
-
-switch (f.get("ddRecycle")) {
-    case "always": base += 1; break;
-    case "regularly": base += 0.5; break;
-    case "sometimes": base += 0.2; break;
-    case "never": base -= 0.5; break;
 }
+function calcBaseEcoScore(){
+    const f = new FormData($("#habitsForm"))
+    let base = 2.5;
 
-switch (f.get("ddEnergy")) {
-    case "green": base += 1; break;
-    case "efficient": base += 0.5; break;
-    case "average": base -= 0.2; break;
-    case "high": base -= 0.5; break;
+    switch (f.get("ddRecycle")) {
+        case "always": base += 1; break;
+        case "regularly": base += 0.5; break;
+        case "sometimes": base += 0.2; break;
+        case "never": base -= 0.5; break;
+    }
+
+    switch (f.get("ddEnergy")) {
+        case "green": base += 1; break;
+        case "efficient": base += 0.5; break;
+        case "average": base -= 0.2; break;
+        case "high": base -= 0.5; break;
+    }
+
+    switch (f.get("ddTransport")) {
+        case "walking": base += 1; break;
+        case "biking": base += 1; break;
+        case "publictransit": base += 1; break;
+        case "mixed": base += 0.2; break;
+        case "car": base -= 1; break;
+    }
+
+    switch (f.get("ddDiet")) {
+        case "vegan": base += 1; break;
+        case "vegetarian": base += 0.5; break;
+        case "omnivore": base += 0; break;
+        case "mostlymeat": base -= 0.5; break;
+        case "carnivore": base -= 1; break;
+
+    }
+
+    switch (f.get("ddShower")) {
+        case "10": base += 0.6; break;
+        case "20": base += 0.4; break;
+        case "30": base += 0.2; break;
+        case "40": base += 0; break;
+        case "50": base -= 0.2; break;
+        case "60": base -= 0.4; break;
+    }
+
+    return base;
 }
-
-switch (f.get("ddTransport")) {
-    case "walking": base += 1; break;
-    case "biking": base += 1; break;
-    case "publictransit": base += 1; break;
-    case "mixed": base += 0.2; break;
-    case "car": base -= 1; break;
-}
-
-switch (f.get("ddDiet")) {
-    case "vegan": base += 1; break;
-    case "vegetarian": base += 0.5; break;
-    case "omnivore": base += 0; break;
-    case "mostlymeat": base -= 0.5; break;
-    case "carnivore": base -= 1; break;
-
-}
-
-switch (f.get("ddShower")) {
-    case "10": base += 0.6; break;
-    case "20": base += 0.4; break;
-    case "30": base += 0.2; break;
-    case "40": base += 0; break;
-    case "50": base -= 0.2; break;
-    case "60": base -= 0.4; break;
-}
-
-console.log(base);
+$("#habitsForm").addEventListener("input", () => {
+    console.log(calcBaseEcoScore());
+});
